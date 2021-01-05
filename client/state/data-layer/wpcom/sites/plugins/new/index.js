8f0d63fc70596c216cd43168600dcc6693f29f3e
Marin Atanasov <tyxla@abv.bg>
2021-01-05 11:08:42 +0200

    State: Replace flux plugins with Redux in plugin upload

--- client/state/data-layer/wpcom/sites/plugins/new/index.js
+++ client/state/data-layer/wpcom/sites/plugins/new/index.js
@@ -8,8 +8,8 @@ import { find, includes } from 'lodash';
 /**
  * Internal dependencies
  */
-import { PLUGIN_UPLOAD } from 'calypso/state/action-types';
-import { PLUGIN_UPLOAD as PLUGIN_UPLOAD_ACTION } from 'calypso/lib/plugins/constants';
+import { PLUGIN_INSTALL_REQUEST_SUCCESS, PLUGIN_UPLOAD } from 'calypso/state/action-types';
+import { INSTALL_PLUGIN } from 'calypso/lib/plugins/constants';
 import {
 	completePluginUpload,
 	pluginUploadError,
@@ -19,8 +19,6 @@ import { dispatchRequest } from 'calypso/state/data-layer/wpcom-http/utils';
 import { http } from 'calypso/state/data-layer/wpcom-http/actions';
 import { errorNotice } from 'calypso/state/notices/actions';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import { getSite } from 'calypso/state/sites/selectors';
-import Dispatcher from 'calypso/dispatcher';
 
 import { registerHandlers } from 'calypso/state/data-layer/handler-registry';
 
@@ -66,10 +64,8 @@ const showErrorNotice = ( error ) => {
 	return errorNotice( translate( 'Problem installing the plugin.' ) );
 };
 
-export const uploadComplete = ( { siteId }, data ) => ( dispatch, getState ) => {
+export const uploadComplete = ( { siteId }, data ) => ( dispatch ) => {
 	const { slug: pluginId } = data;
-	const state = getState();
-	const site = getSite( state, siteId );
 
 	dispatch(
 		recordTracksEvent( 'calypso_plugin_upload_complete', {
@@ -79,15 +75,12 @@ export const uploadComplete = ( { siteId }, data ) => ( dispatch, getState ) => 
 
 	dispatch( completePluginUpload( siteId, pluginId ) );
 
-	/*
-	 * Adding plugin to legacy flux store provides data for plugin page
-	 * and displays a success message.
-	 */
-	Dispatcher.handleServerAction( {
-		type: 'RECEIVE_INSTALLED_PLUGIN',
-		action: PLUGIN_UPLOAD_ACTION,
-		site,
-		plugin: data,
+	// Provides data for plugin page and displays a success message.
+	dispatch( {
+		type: PLUGIN_INSTALL_REQUEST_SUCCESS,
+		action: INSTALL_PLUGIN,
+		siteId,
+		pluginId,
 		data,
 	} );
 };
